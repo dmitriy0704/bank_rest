c51f3b6a900e9d5c0ae6e336023f0c6fdf8dd4c4
Dmitriy Folomkin <dmitriy.folomkin@gmail.com>
2025-11-25 13:25:51 +0300

    Починил тест для TestContainers создания и получения карты

--- src/test/java/dev/folomkin/bankrest/testcontainers/CardControllerTestCntrs.java
+++ src/test/java/dev/folomkin/bankrest/testcontainers/CardControllerTestCntrs.java
@@ -1,47 +1,41 @@
 package dev.folomkin.bankrest.testcontainers;
 
 
-import dev.folomkin.bankrest.config.JwtTokenProvider;
 import dev.folomkin.bankrest.domain.dto.card.CardRequest;
 import dev.folomkin.bankrest.domain.dto.card.CardResponse;
-import dev.folomkin.bankrest.domain.dto.user.UserResponse;
 import dev.folomkin.bankrest.domain.model.Card;
 import dev.folomkin.bankrest.domain.model.CardStatus;
 import dev.folomkin.bankrest.domain.model.Role;
 import dev.folomkin.bankrest.domain.model.User;
 import dev.folomkin.bankrest.repository.UserRepository;
 import dev.folomkin.bankrest.service.security.JwtService;
-import org.junit.jupiter.api.BeforeEach;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.*;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
-import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.List;
 
-import static io.restassured.RestAssured.when;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@Slf4j
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, properties =
         "spring.security.enabled=false")
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -56,6 +50,12 @@ public class CardControllerTestCntrs {
 
     @Autowired
     private JwtService jwtService; // ← Авто-инжект из контекста
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17")
@@ -75,61 +75,59 @@ public class CardControllerTestCntrs {
     }
 
 
-
-
     @Test
-    @WithMockUser(username = "John Doe", roles = {"ADMIN"})
-    void shouldCreateAndGetCard() {
-
-        // Given: Генерируем JWT-токен для user с ролью USER
-        String token = jwtService.generateToken("John Doe", Arrays.asList("ROLE_ADMIN")); // Или ROLE_USER, в зависимости от вашего формата
-
-        // Headers с Bearer
-        HttpHeaders headers = new HttpHeaders();
-        headers.setBearerAuth(token);
-        restTemplate = restTemplate.withBasicAuth("", ""); // Очистка, если нужно
-
+    void shouldCreateAndGetCard() throws JsonProcessingException {
         var user = new User();
-        user.setId(1L);
         user.setUsername("John Doe");
         user.setPassword("password");
         user.setEmail("email@email.com");
         user.setCreatedAt(LocalDateTime.now());
-        user.setRole(Role.ROLE_USER);
+        user.setRole(Role.ROLE_ADMIN);
+        userRepository.save(user);
 
+        log.info("Сохранился пользователь с Id {}", user.getId());
 
+        // Given: Генерируем JWT-токен для user с ролью USER
+        String token = jwtService.generateToken("John Doe", List.of("ROLE_ADMIN")); // Или ROLE_USER, в зависимости от вашего формата
 
-        Card newCard = new Card(
-                1L,
+        // Headers с Bearer
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(token);
+        headers.setContentType(MediaType.APPLICATION_JSON); // Для JSON body
+        restTemplate = restTemplate.withBasicAuth("", ""); // Очистка, если нужно
+
+        CardRequest cardRequest = new CardRequest(
                 "0000 0000 0000 0009",
-                "**** **** **** 0009",
-                LocalDate.now(),
-                CardStatus.ACTIVE,
+                LocalDate.of(2026, 10, 30), // Future дата для @Future
                 BigDecimal.valueOf(123.4),
-                user);
+                user.getId() // ← userId из saved user
+        );
+        String json = objectMapper.writeValueAsString(cardRequest); // JSON для body
+        log.info("Request JSON: {}", json); // Debug
 
         // When: POST создание
-        HttpEntity<Card> requestEntity = new HttpEntity<>(newCard, headers);
-        ResponseEntity<Card> createResponse = restTemplate.exchange(
+        HttpEntity<String> requestEntity = new HttpEntity<>(json, headers);
+        ResponseEntity<CardResponse> createResponse = restTemplate.exchange(
                 "http://localhost:" + port + "/api/v1/cards/create-card",
                 HttpMethod.POST,
                 requestEntity,
-                Card.class
+                CardResponse.class
         );
 
         // Then: 200 OK
-        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.OK);
-        Card savedCard = createResponse.getBody();
-        assertThat(savedCard.getId()).isGreaterThan(1L);
-
+        assertThat(createResponse.getStatusCode()).isEqualTo(HttpStatus.CREATED); // Или OK
+        CardResponse savedCard = createResponse.getBody();
+        assertThat(savedCard).isNotNull();
+        Assertions.assertNotNull(savedCard);
+        assertThat(savedCard.id()).isGreaterThan(0L); // Генерируется БД
 
         // GET с тем же токеном
         HttpEntity<Void> getEntity = new HttpEntity<>(headers);
-        ResponseEntity<Card> getResponse = restTemplate.exchange(
-                "http://localhost:" + port + "/api/v1/cards/card-id/{cardId}" + savedCard.getId(),
+        ResponseEntity<CardResponse> getResponse = restTemplate.exchange(
+                "http://localhost:" + port + "/api/v1/cards/card-id/" + savedCard.id(),
                 HttpMethod.GET,
                 getEntity,
-                Card.class
+                CardResponse.class
         );
 
         // Then: Проверяем получение
